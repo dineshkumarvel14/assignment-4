cb3839cc9aaca3d0e3f22bba7b75d76cf44c4389
dineshkumarvel14 <92155586+dineshkumarvel14@users.noreply.github.com>
2022-07-25 19:14:10 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+   region = "eu-west-2"
+   access_key = "AKIAROGGUQX7DFZ6AY3M"
+   secret_key = "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"
+ }
+
+
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+main_vpc_cidr = "10.0.0.0/32"
+ public_subnets = "10.0.0.128/32"
+ private_subnets = "10.0.0.192/32"
